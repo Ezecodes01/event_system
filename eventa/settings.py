20ac4b6f2601e84405bd0f545ece7be4d678b832
Ezecode01 <chikwendujohnpaul6@gmail.com>
2025-10-09 22:55:43 +0100

    safeguard sec keys

--- eventa/settings.py
+++ eventa/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True  if os.getenv('DEBUG').lower() == 'true' else False
+DEBUG = True  #if os.getenv('DEBUG').lower() == 'true' else False
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOST', 'localhost').strip().split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').strip().split(',')
 
 
 # Application definition
